24750e9ad5900132b0a73e7b7a14ba2638e704c1
gulyolik <olia.guliaeva@yandex.ru>
2022-11-13 00:33:01 +0300

    The second commit

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void findNewMax(){
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10, 20};
+        long expected = 20;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+}
